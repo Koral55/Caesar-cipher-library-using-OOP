5c36f6186e7b85c413bfa653563d7c0bcca0b28d
Koral55 <64688737+Koral55@users.noreply.github.com>
2024-01-02 23:32:00 +0100

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,39 +1,17 @@
-class Character:
-    def __init__(self, character):
-        self.character = character
-        self.unicode = ord(self.character)
-
-
 class Caesar:
     def __init__(self):
         pass
 
     @classmethod
-    def assign(cls, text):
-        list_of_characters = list(text)
-        global objects
-        objects = []
-        for x in list_of_characters:
-            a = Character(x)
-            objects.append(a)
+    def code(cls, input_text, shift=1):
+        coded_message = ""
+        for x in input_text:
+            coded_message += chr(ord(x) + shift)
+        return coded_message
 
     @classmethod
-    def code(cls, text, shift=1):
-        cipher_list = []
-        cls.assign(text)
-        for x in objects:
-            x.unicode += shift
-            cipher_list.append(chr(x.unicode))
-            cipher = "".join(cipher_list)
-        return cipher
-
-    @classmethod
-    def decode(cls, text, shift=1):
-        uncoded_cipher_list = []
-        cls.assign(text)
-        for x in objects:
-            x.unicode -= shift
-            uncoded_cipher_list.append(chr(x.unicode))
-            uncoded_cipher = "".join(uncoded_cipher_list)
-        return uncoded_cipher
-
+    def decode(cls, input_code, shift=1):
+        decoded_message = ""
+        for x in input_code:
+            decoded_message += chr(ord(x) - shift)
+        return decoded_message
